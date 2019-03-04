f33a46a1cbf9a4aea2411cbf73be814a54fc1667
Hólmfríður Magnea Hákonardóttir <holmfridur@Holmfriurs-MacBook-Pro.local>
2019-03-04 20:31:27 +0000

    hofi

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function doAjax() {
     //Prepare the parameter value for 'myParam'
-    var paramValue = document.forms.selection.difficulty.value;
+    paramValue = document.getElementById("difficultySelector").value;
     console.log(paramValue)
     //The URL to which we will send the request
     var url = 'https://veff213-sudoku.herokuapp.com/api/v1/sudoku';
@@ -10,7 +10,7 @@ function doAjax() {
         .then(function (response) {
             //When successful, print 'Success: ' and the received data
             console.log("Success: ", response.data);
-            printBoard(response.data.board.boxes);
+            printBoard(response.data.board.boxes, response.data.board._id);
         })
         .catch(function (error) {
             //When unsuccessful, print the error.
@@ -24,20 +24,23 @@ function doAjax() {
 
 function printBoard(board, sudokuid) {
     var i, n, line, name;
+//    document.getElementById("sudokuForm").style.display = "grid"
     for (i=0; i < board.length; i++) {
         for (n=0; n < board[i].length; n++) {
-            name = "square"+i+"."+n
+            name = "cell"+i+n
             if (board[i][n] != "."){
                 document.getElementById(name).className = "fill";
-                document.getElementById(name).innerText = board[i][n];
+                document.getElementById(name).value = board[i][n];
             } else {
                 document.getElementById(name).className = "input_field";
             }
-            
         }
     }
-    document.getElementById("sudoku_id").innerText = sudokuid
-    //document.getElementById('board').innerText = board
+    document.getElementById("sudokuId").innerText = sudokuid
+};
+
+function validateBoard(){
+    console.log("validate");
 };
 
 // defult board easy: [[],[],[],[],[],[],[],[],[]]
